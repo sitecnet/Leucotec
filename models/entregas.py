a439aa405628e04b3c19448985437873b4d26ba0
Squall-The-Crow <mzamora@sitecnet.com>
2024-02-14 14:29:04 -0600

    arreglando imagen

--- models/entregas.py
+++ models/entregas.py
@@ -9,6 +9,7 @@ class entregas(models.Model):
     name = fields.Char('Numero de pedido', required=True,)
     usuario = fields.Many2one('res.users', string='Usuario', default=lambda self: self.env.user)
     image_1024 = fields.Image("Image", max_width=1024, max_height=1024)
+    image_1920 = fields.Image("Image", max_width=1024, max_height=1024)
     firma = fields.Binary('Firma')
     comentarios = fields.Binary('Comentarios')
     comentarios2 = fields.Text('Comentarios')
